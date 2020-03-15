64f9b1b7d9fe31ecf621c9c9750f38260224e342
Byron Walker-Mills <bwalkermills@gmail.com>
2020-03-15 20:06:02 +0000

    Add checkout

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -10,8 +10,11 @@
     >
     <ul v-else>
       <li v-for="product in products">
-        {{ product.title }} - {{ product.price }} - {{ product.inventory }}
-        <button @click="addProductToCart(product)">Add to cart</button>
+        {{ product.title }} - {{ product.price | currency }} - {{ product.inventory }}
+        <button
+          :disabled="!productIsInStock(product)"
+          @click="addProductToCart(product)"
+        >Add to cart</button>
       </li>
     </ul>
   </div>
@@ -27,7 +30,11 @@
 
     computed: {
       products () {
-        return this.$store.getters.availableProducts
+        return this.$store.state.products
+      },
+
+      productIsInStock () {
+        return this.$store.getters.productIsInStock
       }
     },
 
